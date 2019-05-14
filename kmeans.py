4576a26d35d8153dfc4a191f488bd686dcc8bc5b
Gitdeon <Gitdeon@github.com>
2019-05-14 21:07:54 +0200

    update kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -1,5 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
+from sklearn.datasets.samples_generator import make_blobs
 
-#Todo
+X, y_true = make_blobs(n_samples=2020, centers=4, cluster_std=0.60, random_state=0)
+plt.scatter(X[:, 0], X[:, 1], s=3);
+plt.show()
+plt.close()
+
+kmeans = KMeans(n_clusters=4)
+kmeans.fit(X)
+y_kmeans = kmeans.predict(X)
+
+centers = kmeans.cluster_centers_
+plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5);
+plt.scatter(X[:, 0], X[:, 1], c=y_kmeans, s=3, cmap='viridis')
+plt.show()
+
+
+
+
