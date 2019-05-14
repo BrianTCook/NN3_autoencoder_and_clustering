9417b8e67c0b29f73d5251ef7809f829cf7bdba6
Brian Cook <46697681+BrianTCook@users.noreply.github.com>
2019-05-14 23:06:21 +0200

    Update getinputvectors.py

--- getinputvectors.py
+++ getinputvectors.py
@@ -19,9 +19,11 @@ raw = pd.read_csv('all_stocks_5yr.csv')
 data = raw.loc[:, ('date','open','high','low','close','volume','Name')]
 
 def inputvec(company, year):
+    
     '''
     takes the relevant info from data (company is a string, year is an int)
     generates a 4D vector Q_{i} for the ith quarter
+    
     Q_{i} elements:
         v_{i}: standard deviation of opening price
         s_{i}: mean(high price - low price)
@@ -30,18 +32,27 @@ def inputvec(company, year):
     
     output is concatenation of company, year, Q_{1}, Q_{2}, Q_{3}, and Q_{4}
     '''
+
     #filters out by the company and relevant year
     filter1 = data.loc[data['Name'] == company]
     filter2 = filter1.loc[filter1['date'].str.contains(str(year))]
     
     def Q(i):
+        
         if i == 1:
+            
             month_strs = ['-01-', '-02-', '-03-']
+            
         if i == 2:
+            
             month_strs = ['-04-', '-05-', '-06-']
+            
         if i == 3:
+            
             month_strs = ['-07-', '-08-', '-09-']
+            
         if i == 4:
+            
             month_strs = ['-10-', '-11-', '-12-']
         
         filterquarter = filter2
@@ -61,13 +72,26 @@ def inputvec(company, year):
         
         spreads = [highs[i] - lows[i] for i in range(len(highs))]
         
+        #ignores NaNs
+        
         if len(opens) != 0: #checking if there's actually any data
+            
+            mo, mh, ml, mv, ms = max(opens), max(highs), max(lows), max(volumes), max(spreads)
+        
+            opens = [opening/mo for opening in opens]
+            highs = [high/mh for high in highs]
+            lows = [low/ml for low in lows]
+            volumes = [volume/mv for volume in volumes]
+            spreads = [spread/ms for spread in spreads]
+            
             v, s = np.nanstd(opens), np.nanmean(spreads)
             n, h = np.nanmean(volumes), np.nanmean(highs)
-        
+    
         else:
             v, s, n, h = 'nan', 'nan', 'nan', 'nan'
+    
         return [v, s, n, h]
+
     return [company, year] + Q(1) + Q(2) + Q(3) + Q(4)
 
 companies = data.Name.unique()
@@ -76,7 +100,7 @@ years = ['2014', '2015', '2016', '2017']
 #list comprehension to get input vectors for each company in each year
 inputvecs = [inputvec(company, year) for company in companies for year in years]
 
-#with open('inputvector_table.txt', 'w+') as f:
-#    for item in inputvecs:
-#        if 'nan' not in tuple(item): #doesn't write in vectors with insufficient data
-#            f.write('%s %s %.02f %.02f %.02f %.02f %.02f %.02f %.02f %.02f %.02f %.02f %.02f %.02f %.02f %.02f %.02f %.02f\n'%(tuple(item)))
+with open('inputvector_table.txt', 'w+') as f:
+    for item in inputvecs:
+        if 'nan' not in tuple(item): #doesn't write in vectors with insufficient data
+            f.write('%s %s %.02f %.02f %.02f %.02f %.02f %.02f %.02f %.02f %.02f %.02f %.02f %.02f %.02f %.02f %.02f %.02f\n'%(tuple(item)))
