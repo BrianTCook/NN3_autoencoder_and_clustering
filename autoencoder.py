b4d1b66e40c604859c299021332503e86e6a45d9
Gitdeon <Gitdeon@github.com>
2019-05-15 14:42:00 +0200

    update and added encoded vectors with labels

--- autoencoder.py
+++ autoencoder.py
@@ -1,50 +1,74 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from keras.layers import Input, Dense
 from keras.models import Model
 from keras import optimizers
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import normalize
 
+#load and prepare data
 input_vecs = np.array(pd.read_csv("inputvector_table.txt", sep =" ", header =None))
+vectors = input_vecs[:,2:]
+labels = input_vecs[:,:2]
 
-x_train, x_test, y_train, y_test = train_test_split(input_vecs[:,2:], input_vecs[:,:2], test_size=0.33, random_state=1)
-
-'''
-    We could use normalised vectors: does this make sense?
-    
-    xtrain = normalize(x_train, axis=0, norm='max')
-    xtest = normalize(x_test, axis=0, norm='max')
-'''
+x_train, x_test, y_train, y_test = train_test_split(vectors, labels, test_size=0.33, random_state=1)
 
 #Setup autoencoder structure
 input_vector = Input(shape=(16,)) #Use 16 floats vector as input
 encoded_1 = Dense(500, activation='selu')(input_vector)
 encoded_2 = Dense(250, activation='selu')(encoded_1)
-middle_layer = Dense(2, activation='selu')(encoded_2)
+middle_layer = Dense(2, activation='elu')(encoded_2)
 decoded_1 = Dense(250, activation='selu')(middle_layer)
 decoded_2 = Dense(500, activation='selu')(decoded_1)
-decoded_output = Dense(16, activation='linear')(decoded_2)
+decoded_output = Dense(16, activation='sigmoid')(decoded_2)
 
 '''
     Note: I have been playing around with all kinds of activation
     functions above, as well as optimizers and loss functions below.
     Please share your thoughts on which would be reasonable to use.
+    
+    Nadam and rmsprop seem to have the most promising results.
 '''
 
 autoencoder = Model(input_vector, decoded_output)
-autoencoder.compile(optimizer='adamax', loss='mse')
+autoencoder.compile(optimizer='rmsprop', loss='mae')
 autoencoder.summary()
 
 #Train the autoencoder
 autoencoder.fit(x_train, x_train,
-                epochs=250, #also used many different epoch values/batch sizes
-                batch_size=8,
+                epochs=30, #also used many different epoch values/batch sizes
+                batch_size=4,
                 shuffle=True,
                 validation_data=(x_test, x_test))
 
+autofitted = autoencoder.predict(vectors)
 
-autofitted = autoencoder.predict(x_train)
+#compare to estimate performance
+autofitted[1]
+x_test[1]
 
-autofitted[1] - x_train[1] #compare one input with output for rough effectiveness interpretation
+#separate encoder
+encoder = Model(input_vector, middle_layer)
+encoder.compile(optimizer='rmsprop', loss='mae')
+
+#predict encoded vectors from input vectors
+encoded_vecs = encoder.predict(vectors)
+
+#separate decoder
+encoded_vector = Input(shape=(2,))
+deco1 = autoencoder.layers[-3](encoded_vector)
+deco2 = autoencoder.layers[-2](deco1)
+decoout = autoencoder.layers[-1](deco2)
+decoder = Model(encoded_vector, decoout)
+
+#predict decoded vectors from encoded vectors
+decoded_vecs = decoder.predict(encoded_vecs)
+
+#Confirm that separate encoding/decoding does the same as the autoencoder
+np.array_equal(autofitted, decoded_vecs)
+
+#Write labels with corresponding encoded values to file
+output = np.concatenate((labels,encoded_vecs), axis=1)
+np.savetxt("encoded_with_labels.txt", output, fmt='%s', delimiter=',')
 
--- kmeans.py
+++ kmeans.py
@@ -1,22 +1,35 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.datasets.samples_generator import make_blobs
 
-X, y_true = make_blobs(n_samples=2020, centers=4, cluster_std=0.60, random_state=0)
-plt.scatter(X[:, 0], X[:, 1], s=3);
+#load and prepare data
+data = np.array(pd.read_csv('encoded_with_labels.txt', sep = ',', header=None))
+encoded_values = data[:,2:4]
+
+labels = np.array([])
+for i in range(len(data)):
+    labels = np.append(labels, data[i,0] + ' ' + str(data[i,1]))
+
+plt.scatter(encoded_values[:,0], encoded_values[:,1], s=3)
 plt.show()
 plt.close()
 
-kmeans = KMeans(n_clusters=4)
-kmeans.fit(X)
-y_kmeans = kmeans.predict(X)
-
+#apply kmeans clustering and plot
+kmeans = KMeans(n_clusters=)
+kmeans.fit(encoded_values)
+y_kmeans = kmeans.predict(encoded_values)
 centers = kmeans.cluster_centers_
-plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5);
-plt.scatter(X[:, 0], X[:, 1], c=y_kmeans, s=3, cmap='viridis')
+plt.scatter(centers[:, 0], centers[:, 1], c='black', s=50, alpha=0.5);
+plt.scatter(encoded_values[:, 0], encoded_values[:, 1], c=y_kmeans, s=5, cmap='Dark2')
+plt.text(encoded_values[:, 0], encoded_values[:, 1], labels, fontsize=8)
 plt.show()
 
-
-
-
+#show scatter with all labels
+for i,label in enumerate(labels):
+    x = encoded_values[:,0][i]
+    y = encoded_values[:,1][i]
+    plt.scatter(x, y, s=4)
+    plt.text(x, y, label, fontsize=5)
+plt.show()
